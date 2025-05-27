ffa53d2d3b7835beb32109c2d141a44de0d37c57
ibiruai <48332831+ibiruai@users.noreply.github.com>
2025-05-27 18:02:39 +0300

    Add solution for homework 001-http and 002-jquery (self-check)

---
+++ web-apps/002-jquery/form_ajax.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  $("form").submit(function (event) {
+    var formData = {
+      user_name: $("#user_name").val(),
+      user_second_name: $("#user_second_name").val(),
+      user_last_name: $("#user_last_name").val()
+    };
+
+    $.ajax({
+      type: "POST",
+      url: "dadata.php",
+      data: formData,
+      dataType: "json",
+      encode: true,
+    }).done(function (result) {
+      console.log(result);
+      for (let value of result) {
+          $("#result").html(
+              '<p>Полное имя ' + value.result + '</p><p>В лице ' + value.result_genitive +
+              '</p><p>Кому ' +value.result_dative + '</p><p>Согласовано ' + value.result_ablative +
+              '</p><p>Фамилия ' + value.surname + '</p><p>Имя ' + value.name +
+              '</p><p>Отчество ' + value.patronymic + '</p>' 
+            );
+          console.log('ФИО: ' + value + '\n' );
+      }
+    });
+
+    event.preventDefault();
+  });
+});
